abd0b4bcde2e1ff280612f0ebee417fc416903a5
MonkeyDSamuel <samueleternityunofficial@gmail.com>
2025-08-25 12:10:08 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+from db.db_connection import DBConnection
+def main():
+    while True:
+        print("---------WELCOME TO CLINIC MANAGEMENT SYSTEM---------")
+
+if __name__ == "__main__":
+    main()
